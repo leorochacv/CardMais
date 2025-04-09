5b2f486b8e39fe224c493fff7d258348a03e8c90
Leonardo Rocha <143648993+leorochacv@users.noreply.github.com>
2025-04-09 08:17:42 -0300

    Update verificarPagamento.php

--- pagamentos/verificarPagamento.php
+++ pagamentos/verificarPagamento.php
@@ -29,7 +29,7 @@ $curl = curl_init();
             CURLOPT_CUSTOMREQUEST => "GET",
             CURLOPT_HTTPHEADER => [
                 "accept: application/json",
-                "access_token: \$aact_YTU5YTE0M2M2N2I4MTliNzk0YTI5N2U5MzdjNWZmNDQ6OjAwMDAwMDAwMDAwMDAwNzYzODI6OiRhYWNoXzFmYjg5Y2EzLTIxYWMtNDBlZC1hMDQyLTJjM2RhMGU3YzU2MQ=="
+                "access_token: "
             ],
         ]);
         curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
